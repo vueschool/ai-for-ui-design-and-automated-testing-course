348365edddfe4b2a0c37f5a16a5ce96b7fc5ffbe
Daniel Kelly <me@danielkelly.io>
2025-12-23 14:56:42 -0600

    6-use-image-generators-to-brainstorm-designs

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,10 +3,8 @@
 
 <template>
   <div>
-    <UContainer class="sm:border-x border-default pt-10">
-      <AppHeader :links="navLinks" />
-      <slot />
-      <AppFooter />
-    </UContainer>
+    <AppHeader :links="navLinks" />
+    <slot />
+    <AppFooter />
   </div>
 </template>
