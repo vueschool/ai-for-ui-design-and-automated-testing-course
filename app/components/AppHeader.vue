46a99a5b6b1a6fb3fb8b2ac047832feabe4702f2
Daniel Kelly <me@danielkelly.io>
2025-12-30 15:05:15 -0600

    fix: header z-index

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -7,7 +7,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="fixed top-2 sm:top-4 mx-auto left-1/2 transform -translate-x-1/2 z-10">
+  <div class="fixed top-2 sm:top-4 mx-auto left-1/2 transform -translate-x-1/2 z-20">
     <UNavigationMenu
       :items="links"
       variant="link"
